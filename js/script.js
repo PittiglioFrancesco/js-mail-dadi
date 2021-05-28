b301b1cedd83f77531617aff6b40ee8c7ccc690c
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-05-28 16:17:31 +0200

    correzione e inizio bonus

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@
 
 // input
 
-var dadoCPU = (Math.random() * (7 - 1) + 1);
+var dadoCPU = Math.floor(Math.random() * 6 + 1);
 
-var dadoPlayer = (Math.random() * (7 - 1) + 1);
+var dadoPlayer = Math.floor(Math.random() * 6 + 1);
 
 // operazioni
 
